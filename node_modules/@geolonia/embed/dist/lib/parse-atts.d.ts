3f0272da0e6efc45003e4416d91988c348ed47c9
komasn <magarisun@gmail.com>
2023-11-03 21:21:10 +0900

    すべて削除して初期状態にもどした

--- node_modules/@geolonia/embed/dist/lib/parse-atts.d.ts
+++
@@ -1,6 +0,0 @@
-import type { EmbedAttributes } from '../embed';
-type ParseAttsParams = {
-    interactive?: boolean;
-};
-declare const _default: (container: any, params?: ParseAttsParams) => EmbedAttributes;
-export default _default;
--- node_modules/@geolonia/embed/dist/lib/render.d.ts
+++
@@ -1,5 +0,0 @@
-import 'maplibre-gl/dist/maplibre-gl.css';
-import '../style.css';
-import GeoloniaMap from './geolonia-map';
-export declare const renderGeoloniaMap: () => void;
-export declare const registerPlugin: (plugin: (map: GeoloniaMap, target: HTMLElement, atts: any) => void) => void;
