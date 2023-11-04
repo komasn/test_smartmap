590ae8704b7eb2f0acc17eca7e9fdfbe6062875c
komasn <magarisun@gmail.com>
2023-11-04 20:27:31 +0900

    全体の構成を変更した
    公開用資材だけを配置することにした

--- node_modules/@geolonia/embed/dist/lib/controls/geolonia-control.d.ts
+++
@@ -1,7 +0,0 @@
-import type { ControlPosition, IControl } from 'maplibre-gl';
-export declare class GeoloniaControl implements IControl {
-    private container;
-    onAdd(): HTMLDivElement;
-    onRemove(): void;
-    getDefaultPosition(): ControlPosition;
-}
--- src/utils/isObjEmpty.ts
+++
@@ -1,5 +0,0 @@
-const isObjEmpty = (obj: any) => {
-  return Object.keys(obj).length === 0;
-}
-
-export default isObjEmpty;
